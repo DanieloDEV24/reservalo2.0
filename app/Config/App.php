9925c349c5cddb545b0a27f33287031799d2081e
DanieloDEV24 <danielruizdeveloper@gmail.com>
2026-04-16 12:19:04 +0200

    responsive

--- app/Config/App.php
+++ app/Config/App.php
@@ -25,10 +25,10 @@ class App extends BaseConfig
     // public string $baseURL = 'http://172.26.1.31/reservalo2.0/';
 
     // BaseURL --> localhost de la casa
-    // public string $baseURL = 'http://192.168.1.45/reservalo2.0/';
+    public string $baseURL = 'http://192.168.1.45/reservalo2.0/';
 
     // BaseURL --> localhost wifi guadalinfo
-    public string $baseURL = 'http://192.168.0.20/reservalo2.0/';
+    // public string $baseURL = 'http://192.168.0.20/reservalo2.0/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
